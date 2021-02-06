e081785d40d2e03e79e72458920bd3ffe65f4c4b
Antony <deviant_kun@protonmail.com>
2021-02-06 20:36:40 +0300

    Add balance checking

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	keysPerPage := 128
+	printBitcoinKeys(keysPerPage)
+}
+
+func printBitcoinKeys(keysPerPage int) {
+	compressedKeys := ""
+	bitcoinKeys := generateBitcoinKeys(keysPerPage)
+
+	length := len(bitcoinKeys)
+
+	file, err := os.Create("keys.txt")
+
+	file.Truncate(0)
+
+	if err != nil {
+        log.Fatal(err)
+  	}
+
+	defer file.Close()
+
+	for i, key := range bitcoinKeys {
+		compressedKeys += key.compressed
+
+		if i != length - 1 {
+			compressedKeys += ","
+		}
+	}
+
+	findedBalance := checkBtcBalanceWallet(compressedKeys)
+
+	if findedBalance != "" {
+		for i, key := range bitcoinKeys {
+			if key.compressed == findedBalance {
+				fmt.Println("Wallet found! " + key.private)
+				fmt.Fprintln(file, i, key)
+			}
+		}
+	} else {
+		fmt.Println(time.Now().UTC().Format("15:04:05") + " Nothing found..." + "\n")
+		time.Sleep(1000 * time.Millisecond)
+		printBitcoinKeys(keysPerPage)
+	}
+}
+
+func checkBtcBalanceWallet(compressed string) string {
+	resp, err := http.Get("https://blockchain.info/balance?cors=true&active=" + compressed)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	type Data struct {
+		final_balance int
+		n_tx int
+		total_received int
+	}
+
+	var result map[string]Data
+
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for i, key := range result {
+		if key.final_balance != 0 {
+			return i
+		}
+	}
+
+	return ""
+}
